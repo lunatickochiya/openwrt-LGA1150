22892c63e5d67090750e337e740e585fb98e0f9c
LGA1150 <dqfext@gmail.com>
2019-10-21 18:52:13 +0800

    ipq40xx: add support for P&W R619AC

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -71,6 +71,8 @@ platform_do_upgrade() {
 	engenius,eap2200 |\
 	luma,wrtq-329acn |\
 	mobipromo,cm520-79f |\
+	p2w,r619ac-64m |\
+	p2w,r619ac-128m |\
 	qxwlan,e2600ac-c2)
 		nand_do_upgrade "$1"
 		;;
