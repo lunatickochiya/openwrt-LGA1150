d437e973778f170e14bdf80c33a889441342ae95
Aodzip <aodzip@gmail.com>
2021-08-11 16:51:34 +0800

    ramips: add HATLab-GateBoard-M1

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -9,6 +9,15 @@ RAMFS_COPY_BIN='fw_printenv fw_setenv'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
 platform_check_image() {
+	local board=$(board_name)
+
+	case "$board" in
+	hatlab,gateboard-m1)
+		hatlab_check_image "$1"
+		return $?;
+		;;
+	esac
+
 	return 0
 }
 
@@ -114,6 +123,9 @@ platform_do_upgrade() {
 		echo "Kernel2 sequence number was reset to 0"
 		CI_KERNPART="Kernel"
 		nand_do_upgrade "$1"
+		;;
+	hatlab,gateboard-m1)
+		hatlab_do_upgrade "$1"
 		;;
 	*)
 		default_do_upgrade "$1"
